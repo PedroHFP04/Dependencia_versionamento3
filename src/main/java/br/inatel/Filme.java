ed89a1b87e6314dbdce07ed983f6cea76bf6d76b
luizOtpaiva <luizotavio.paiva04@gmail.com>
2025-08-19 16:57:48 -0300

    adicionei diretor

--- src/main/java/br/inatel/Filme.java
+++ src/main/java/br/inatel/Filme.java
@@ -1,13 +1,18 @@
 package br.inatel;
 
 public class Filme {
-    String titulo;
-    int ano;
-    String genero;
+    private String titulo;
+    private int ano;
+    private String genero;
+    private String diretor;
+    
 
-    public Filme( String titulo, int ano, String genero ) {
+    public Filme( String titulo, int ano, String genero, String diretor) {
         this.titulo = titulo;
         this.ano = ano;
         this.genero = genero;
+        this.diretor = diretor;
     }
+
+
 }
